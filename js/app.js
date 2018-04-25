370fd0e5ad8619754ae1529bb57132d0a1f608c9
Adam Klepacz <adamklepacz92@gmail.com>
2018-04-25 12:34:19 +0200

    form API implementation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function () {
 
+  // DOM elements handlers
   const registerLink = document.getElementById('nav__register');
   const loginLink = document.getElementById('nav__login');
   const registerModal = document.getElementById('register__modal');
@@ -8,6 +9,14 @@
   const overlay = document.getElementById('overlay');
   const registerForm = document.getElementById('register__form');
   const loginForm = document.getElementById('login__form');
+
+  // form inputs
+  const email = document.getElementById('register__email');
+  const password = document.getElementById('register__password');
+  const phone = document.getElementById('register__phone');
+  const refCode = document.getElementById('register__ref__code');
+  const picture = document.getElementById('register__picture');
+
 
   function triggerModal(e) {
     const clickedElement = e.target.attributes.id.value;
@@ -41,12 +50,15 @@
     }
   }
 
+
   /**
    * Html5 validation is complete so we can submit the form
    * or we can build our own validation in JavaScript if needed
    */
-  function formSubmit() {
-    renderHTML(registerModal, '<h4 class="register__heading">Register</h4><p>Registration complete, check your email for activation link!</p><p>Click anywhere to close this message.</p>');
+  function formSubmit(e) {
+    // e.preventDefault();
+
+    // renderHTML(registerModal, '<h4 class="register__heading">Register</h4><p>Registration complete, check your email for activation link!</p><p>Click anywhere to close this message.</p>');
 
     registerForm.reset();
   }
@@ -88,3 +100,42 @@
 
 
 }());
+
+
+
+// below functionality works only with CORS
+
+// function getFormData(e) {
+//   e.preventDefault();
+//   const emailData = email.value;
+//   const passwordData = password.value;
+//   const phoneData = phone.value;
+//   const refData = refCode.value;
+//   const pictureData = picture.value;
+
+//   formSubmit(e, emailData, passwordData, phoneData, refData, pictureData);
+// }
+
+// /**
+//  * Html5 validation is complete so we can submit the form
+//  * or we can build our own validation in JavaScript if needed
+//  */
+// function formSubmit(e, email, password, phone, refCode, picture) {
+//   let formData = new FormData();
+//   formData.append("email", email);
+//   formData.append("password", password);
+//   formData.append("phone", phone);
+//   formData.append("refferal_code", refCode);
+//   formData.append("picture", picture);
+
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", "https://foodsi-stage.applover.pl//api/v1/auth");
+//   xhr.send(formData);
+
+//   renderHTML(registerModal, '<h4 class="register__heading">Register</h4><p>Registration complete, check your email for activation link!</p><p>Click anywhere to close this message.</p>');
+
+//   registerForm.reset();
+// }
+
+
+// registerForm.addEventListener('submit', getFormData);
